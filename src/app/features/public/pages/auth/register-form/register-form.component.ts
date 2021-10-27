84924608f8118e8dc7081ad9bd9b2188412d69d5
Luciano <feyaluciano@gmail.com>
2021-10-27 14:19:56 -0300

    I validated

--- src/app/features/public/pages/auth/register-form/register-form.component.ts
+++ src/app/features/public/pages/auth/register-form/register-form.component.ts
@@ -1,46 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { User } from '../../../models/User';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { passwordMatchValidator } from "src/app/shared/utils/password-match-validator";
+import { User } from "../../../models/User";
 
 @Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  selector: "app-register-form",
+  templateUrl: "./register-form.component.html",
+  styleUrls: ["./register-form.component.scss"],
 })
 export class RegisterFormComponent implements OnInit {
-  form:FormGroup;
-  editing:boolean=false;
-  constructor(private _builder:FormBuilder,private router:Router) {
-    this.form=this._builder.group({
-      Email:['',[Validators.required,Validators.email]],
-      Password: ['', [Validators.required,Validators.pattern(
-        /^(?=.*[A-Za-z])(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/
-        )]],
-      ConfirmPassword: ['', [Validators.required]],   
+  form: FormGroup;
+  editing: boolean = false;
+  passwordsAreEqualValue: boolean = false;
+  constructor(private _builder: FormBuilder, private router: Router) {
+    this.form = this._builder.group({
+      Email: ["", [Validators.required, Validators.email]],
+      Password: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[A-Za-z])(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/
+          ),
+        ],
+      ],
+      ConfirmPassword: ["", [Validators.required]],
     });
-   }
-
-   
-
-save(){
-  //this functionality is temporary and will be replaced by a service
-  if (this.form.valid) {
-        const user:User={"email":this.form.get('Email')?.value,"password":this.form.get('Password')?.value} 
-        localStorage.setItem('userLogged', JSON.stringify(user));
-        this.router.navigate(['/home']);  
-  }
-  else {
-    this.form.markAllAsTouched();
-    
   }
 
-}
+  passwordsAreEqual() {
+    if (
+      this.form.get("Password")?.value ===
+      this.form.get("ConfirmPassword")?.value
+    ) {
+      return true;
+    }
+    return false;
+  }
 
-  
+  save() {
+    this.passwordsAreEqualValue = !this.passwordsAreEqual();
+    if (this.form.valid && this.passwordsAreEqual()) {
+      const user: User = {
+        email: this.form.get("Email")?.value,
+        password: this.form.get("Password")?.value,
+      };
+      localStorage.setItem("userLogged", JSON.stringify(user));
+      this.router.navigate(["/home"]);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 
   ngOnInit(): void {
-    this.editing=false;
+    this.editing = false;
   }
-
 }
