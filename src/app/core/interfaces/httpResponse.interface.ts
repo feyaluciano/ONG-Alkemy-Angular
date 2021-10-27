af70370f25c2b153a2ba020389517baff24d40c7
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-27 10:22:19 -0300

    OT97-53 / interface folder in app/core

---
+++ src/app/core/interfaces/httpResponse.interface.ts
@@ -0,0 +1,18 @@
+export interface HTTPResponse {
+    success: boolean;
+    data:    Datum[];
+    message: string;
+}
+
+export interface Datum {
+    id:          number;
+    name:        string;
+    description: null | string;
+    image:       null | string;
+    order:       number | null;
+    user_id:     null;
+    created_at:  Date;
+    updated_at:  Date;
+    deleted_at:  null;
+    group_id:    number | null;
+}
