13a1e54a489b336d18bfffdeeb10ed78efa7783f
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-28 04:54:41 -0300

    OT97-54 - about component

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -8,9 +8,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
 import { TitlesComponentComponent } from './components/titles-component/titles-component.component';
 import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent,RegisterFormComponent, TitlesComponentComponent, LoginFormComponent],
+  declarations: [HomeComponent,RegisterFormComponent, TitlesComponentComponent, LoginFormComponent, AboutComponent],
   imports: [
     CommonModule,
     PublicRoutingModule,
