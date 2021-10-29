342e4a1d47cc1d819982c3c7bcedc385a405c249
Luciano <feyaluciano@gmail.com>
2021-10-29 00:50:56 -0300

    i developed the activities Crud, and i added sweetalert2

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -16,5 +16,16 @@ export class HttpService {
   public get<T>(url: string, activateHeader:boolean = false ):Observable<T> {
     return this.http.get<T>(url, activateHeader ? { headers: this._headers }: {});
   }
+
+
+  public put<T>(url: string, data: any, activateHeader:boolean = false ):Observable<T> {
+    return this.http.put<T>(url, data, activateHeader ? { headers: this._headers }: {});
+  }
+
+  public post<T>(url: string, data: any, activateHeader:boolean = false ):Observable<T> {
+    return this.http.post<T>(url, data, activateHeader ? { headers: this._headers }: {});
+  }
+
+
 }
 
