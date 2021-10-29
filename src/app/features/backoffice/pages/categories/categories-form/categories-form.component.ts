c0be9668ff789bb3ddc08261f75ce746c30edf23
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-10-29 15:04:44 -0300

    added image field

--- src/app/features/backoffice/pages/categories/categories-form/categories-form.component.ts
+++ src/app/features/backoffice/pages/categories/categories-form/categories-form.component.ts
@@ -18,6 +18,8 @@ export class CategoriesFormComponent implements OnInit {
     ])
   });
 
+  public imageSrc: string = '';
+
   constructor(
     private ckeditorSvc: CkeditorService
   ) {
@@ -35,6 +37,23 @@ export class CategoriesFormComponent implements OnInit {
   onSubmit() {
     
     console.log(this.categoriesForm.value);
+  }
+
+  handleInputChange(e: any) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    var pattern = /image-*/;
+    var reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e: any) {
+    let reader = e.target;
+    this.imageSrc = reader.result;
+    console.log(this.imageSrc)
   }
 
 }
