c105dbadb978212f0180174eb5e2cb95f6605743
Franco Oliva <franco.oliva1991@gmail.com>
2021-10-31 01:42:03 -0300

    OT-97-Server-32 - reactive forms, directive errors in backoffice/home

--- src/app/features/backoffice/pages/backoffice-home/backoffice-home.component.ts
+++ src/app/features/backoffice/pages/backoffice-home/backoffice-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-backoffice-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackofficeHomeComponent implements OnInit {
 
-  constructor() { }
+
+  homeForm: FormGroup = this.fb.group({
+    txtWelcome: [ '',[Validators.required, Validators.minLength(20)]]
+  });
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  errorMsg(controlName: string, error: string){
+    return Boolean( this.homeForm.get(controlName)?.hasError(error) && this.homeForm.get(controlName)?.touched );
+  }
+
+  edit(){
+    console.log(this.homeForm);
+    // console.log('required', this.homeForm.controls['txtWelcome'].hasError('required'));
+    // console.log('minLength', this.homeForm.controls['txtWelcome'].hasError('minlength'));
+    
   }
 
 }
