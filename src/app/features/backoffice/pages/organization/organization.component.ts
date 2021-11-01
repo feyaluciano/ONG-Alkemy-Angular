af76fbb253b4eb06202d75b7d2672e19927c27f2
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-01 08:01:13 -0300

    OT97-34 - ckEditor ok!

--- src/app/features/backoffice/pages/organization/organization.component.ts
+++ src/app/features/backoffice/pages/organization/organization.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-organization',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationComponent implements OnInit {
 
-  constructor() { }
+  organizationForm: FormGroup = this.fb.group({
+    name: [null, Validators.required ],
+    logo: [null, Validators.required ],
+    shortDescription: [null, Validators.required ],
+    longDescription: [null, Validators.required],
+    facebook_url: [null, [Validators.required]],
+    linkedin_url: [null, [Validators.required]],
+    instagram_url: [null, [Validators.required]],
+    twitter_url: [null, [Validators.required]]
+  });
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  errorMsg(controlName: string, error: string){
+    return Boolean( this.organizationForm.get(controlName)?.hasError(error) && this.organizationForm.get(controlName)?.touched );
   }
 
   editOrganization(){
