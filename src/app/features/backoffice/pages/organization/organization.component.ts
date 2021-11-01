1c3d192eb23ad11d785ed0ab8060a99581aa51a1
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-01 07:26:57 -0300

    OT97-34 / organizationComponent

---
+++ src/app/features/backoffice/pages/organization/organization.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization',
+  templateUrl: './organization.component.html',
+  styleUrls: ['./organization.component.scss']
+})
+export class OrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  editOrganization(){
+
+  }
+
+  uploadImg(event:any){
+
+  }
+
+}
