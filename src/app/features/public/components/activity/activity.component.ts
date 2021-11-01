20b53e16215abefa964388ea3e9b671639b44053
Jose Brito <ing.josebritovillarroel@gmail.com>
2021-11-01 13:29:18 -0300

    tarea OT97-58 creacion del componente contenido de una actividad lista

---
+++ src/app/features/public/components/activity/activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity',
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.scss']
+})
+export class ActivityComponent implements OnInit {
+@Input() description:string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
