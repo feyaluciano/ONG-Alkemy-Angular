83b5f39c26f840244fabb1e150a79b83f2af2859
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-03 08:45:30 -0300

    OT97-78/feature/extend-http-users-service - I modified: users interface

--- src/app/features/models/User.ts
+++ src/app/features/models/User.ts
@@ -1,4 +1,17 @@
 export interface User {
-  email?: string;
-  password?: string;
+    id:                number;
+    name:              string;
+    email?:             string;
+    email_verified_at: Date | null;
+    password?:          string;
+    role_id:           number;
+    remember_token:    null | string;
+    created_at:        Date;
+    updated_at:        Date;
+    deleted_at:        null;
+    group_id:          number | null;
+    latitude:          null | string;
+    longitude:         null | string;
+    address:           null | string;
+    profile_image:     null;
 }
