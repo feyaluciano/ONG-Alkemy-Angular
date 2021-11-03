4f22a2d9b0d2ed22004d3f398c76f5799eb67ee2
Luciano <feyaluciano@gmail.com>
2021-11-03 12:12:48 -0300

    removed async into getActibityById

--- src/app/features/backoffice/services/private-backoffice.service.ts
+++ src/app/features/backoffice/services/private-backoffice.service.ts
@@ -11,7 +11,7 @@ export class PrivateBackofficeService {
 
 constructor(private httpService:HttpService,private userStatusService:UserStatusService) {}
 
-async getActivityById(url: string, id: string):Promise<HTTPResponse<Activity>> {
+ getActivityById(url: string, id: string):Promise<HTTPResponse<Activity>> {
     this.httpService.getHeaders().append("Authorization", this.userStatusService.getHeaders());
     const obsActivity$:Observable<HTTPResponse<Activity>> = this.httpService.get(url + id,false);    
     const activityPromise :Promise<HTTPResponse<Activity>> = obsActivity$.toPromise();    
