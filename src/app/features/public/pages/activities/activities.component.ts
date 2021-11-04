d022927c3a9041695024e110c7e45ebf0d0a0422
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-04 14:06:13 -0300

    Deploying the activities component

--- src/app/features/public/pages/activities/activities.component.ts
+++ src/app/features/public/pages/activities/activities.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { ActivitiesService } from '../../../services/activities/activities.service';
+import { Activity } from '../../../models/Activity';
 
 @Component({
   selector: 'app-activities',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activities.component.scss']
 })
 export class ActivitiesComponent implements OnInit {
+  public title: string = 'Actividades';
+  public activities: Activity[] = [];
 
-  constructor() { }
+  constructor(
+    private activitiesService: ActivitiesService
+  ) {
+    this.activitiesService.getActivities('/activities')
+      .subscribe((data: any) => {
+        console.log(data);
+        
+        Swal.fire({
+          title: 'Hecho!',
+          text: data.message,
+          icon: 'success',
+          showConfirmButton: true,
+          timer: 2000
+        });
+
+        this.activities = data.data;
+      });
+  }
 
   ngOnInit(): void {
   }
--- src/app/features/services/activities/activities.service.ts
+++ src/app/features/services/activities/activities.service.ts
@@ -4,14 +4,23 @@ import { UserStatusService } from 'src/app/core/services/user-status.service';
 import { PrivateBackofficeService } from '../../backoffice/services/private-backoffice.service';
 import { Activity } from '../../models/Activity';
 import { HTTPResponse } from '../../models/HTTPResponse';
+import { PublicService } from '../../public/services/public.service';
 
 @Injectable({
   providedIn: "root",
 })
 export class ActivitiesService { 
-  constructor(private privateBackofficeService: PrivateBackofficeService,private userStatusService:UserStatusService) {}
+  constructor(
+    private privateBackofficeService: PrivateBackofficeService,
+    private userStatusService:UserStatusService,
+    private publicService: PublicService
+  ) {}
 
   getActivityById(url:string,id:string):Observable<HTTPResponse<Activity>> {  
     return this.privateBackofficeService.getActivityById(url,id);
   }    
+
+  getActivities(param: string): Observable<HTTPResponse<Activity[]>> {
+    return this.publicService.getEntities(param);
+  }
 }
