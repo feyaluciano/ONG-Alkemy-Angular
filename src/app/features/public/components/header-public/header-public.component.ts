18344f4c96d3fa47a25074d414cdcc778cbf77da
Jose Brito <ing.josebritovillarroel@gmail.com>
2021-11-06 00:39:45 -0300

    se añadio el servicio para el login en el core services con http client, se seteo el token en el localstorage, se aplico directivas en el header public para cambiar el boton (log in, log out) segun el estado del usuario

--- src/app/features/public/components/header-public/header-public.component.ts
+++ src/app/features/public/components/header-public/header-public.component.ts
@@ -18,7 +18,7 @@ export class HeaderPublicComponent implements OnInit {
   logOut(){
     localStorage.removeItem("userToken");
     this.loggedIn = false;
-    this.router.navigate([""])
+    this.router.navigate([''])
   }
 
 }
