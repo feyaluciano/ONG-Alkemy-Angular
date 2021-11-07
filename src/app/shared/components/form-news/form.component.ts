ae95b6f759a53a5df094335f7e2b7904ad8052aa
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-07 11:44:33 -0300

    SweeAlert for invalid form

--- src/app/shared/components/form-news/form.component.ts
+++ src/app/shared/components/form-news/form.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CkeditorService } from 'src/app/core/services/ckeditor.service';
 import { News } from '../../../features/models/news.interface';
 import Swal from 'sweetalert2';
 import { PrivateBackofficeService } from '../../../features/backoffice/services/private-backoffice.service';
 import { Category } from 'src/app/features/models/category.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -28,16 +29,18 @@ export class FormComponent implements OnInit {
     name: ['', [Validators.required, Validators.minLength(4)] ],
     image: ['', Validators.required ],
     category_id: ['', Validators.required ]
-  });
-
-  
+  });  
 
   constructor( 
     private fb: FormBuilder, 
     private ckeditorService: CkeditorService, 
-    private categoriesServices: PrivateBackofficeService ) { }
+    private categoriesServices: PrivateBackofficeService,
+    private activatedRoute: ActivatedRoute ) { }
 
-  ngOnInit(): void {
+  /**             despejar dudas con respecto a la parte de editar una novedad                        */
+
+  ngOnInit(): void {    
+
     this.ckeditorService.getHandlerTextEditor$().subscribe( (r) => { 
 
       this.contentCKeditor = r; 
@@ -47,24 +50,18 @@ export class FormComponent implements OnInit {
     this.categoriesServices.getEntities('http://ongapi.alkemy.org/api/categories').subscribe( categories => {
       let c:any = categories;
       this.categories = c.data;
-      console.log(this.categories);
+      //console.log(this.categories);
     });
 
     
-  }
-
-  
+  }  
 
   uploadImg(event:any){    
-
-    let imgBase64: string = '';
 
     const file = event.target.files[0];
     const reader = new FileReader();
 
-    if(event.target.value){
-
-      
+    if(event.target.value){  
 
       // .png o .jpg save img
       if(file.type == 'image/png' || file.type == 'image/jpg' || file.type == 'image/jpeg'){
@@ -73,14 +70,11 @@ export class FormComponent implements OnInit {
         reader.readAsDataURL(file);
         reader.onload = () => { 
 
-        let imgBase64: string = "data:image/png;base64," + reader.result?.toString().split(',')[1];
       
-        this.imageB64 = imgBase64;
+        this.imageB64 = "data:image/png;base64," + reader.result?.toString().split(',')[1];;
         
       
-        };
-
-        
+        };  
 
       } 
     }
@@ -97,34 +91,37 @@ export class FormComponent implements OnInit {
     let date: string = (new Date()).toLocaleDateString() + " " + (new Date()).toLocaleTimeString();
 
     if(this.contentCKeditor){
-      // nada
+
+      if(this.form.valid){
+      
+        let news: News = {
+          name: this.form.get('name')?.value,
+          content: this.contentCKeditor,
+          image: this.imageB64,
+          category_id: this.form.get('category_id')?.value,
+          created_at: date,
+          updated_at: null,
+          deleted_at: null,
+          slug: null,
+          user_id: null
+        }
+  
+        // petición POST
+        console.log(news);
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: 'Campos incompletos.',
+          icon: 'error'
+        });
+      }
     } else {
-      // sweetAlert
+      // ckEditor error
       Swal.fire({
         title: 'Error',
         text: 'Ingrese una descripción.',
         icon: 'error'
       });
-    }
-
-    
-
-    if(this.form.valid){
-      
-      let news: News = {
-        name: this.form.get('name')?.value,
-        content: this.contentCKeditor,
-        image: this.imageB64,
-        category_id: 1,
-        created_at: date,
-        updated_at: null,
-        deleted_at: null,
-        slug: null,
-        user_id: null
-      }
-
-      // petición POST
-      console.log(news);
     }
     
   }
