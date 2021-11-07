cfdbe7225f95d01ea3685d13d30d3e0f9644e726
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-06 22:29:25 -0300

    attempt to validate form

--- src/app/shared/components/contact-form/contact-form.component.ts
+++ src/app/shared/components/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,47 +10,83 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ContactFormComponent implements OnInit {
 
   @Input() title!: string;
+  form!: FormGroup;
 
-  contactForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
-    phone: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^[0-9]+$/),
-      Validators.minLength(8)
-    ]),
-    message: new FormControl('', [
-      Validators.required
-    ])
-  });
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createForm();
+    switch (this.router.url) {
+      case '/contacto':
+        this.addContactFields();
+        break;
+  
+      default:
+        break;
+    }
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      name: new FormControl('', [
+        Validators.required
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      otherFields: this.formBuilder.array([])
+    });
+  }
+
+  get otherFields(): FormArray {
+    return this.form.get('otherFields') as FormArray;
+  }
+
+  addContactFields() {
+    const contact = this.formBuilder.group({
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9]+$/),
+        Validators.minLength(8)
+      ]),
+      message: new FormControl('', [
+        Validators.required
+      ])
+    });
+
+    this.otherFields.push(contact);
+
+    console.log(this.form.get('otherFields')?.value[0]);
+
+    console.log(this.otherFields.controls[0].get('phone')?.hasError('required'));
+    console.log(this.otherFields.controls[0]);
+    
+    
   }
 
   onSubmit() {
 
     // TODO: Remove console.logs and add logic here
 
-    console.log('name:', this.contactForm.value.name);
+    console.log('name:', this.form.value.name);
 
-    console.log('email:', this.contactForm.value.email);
+    console.log('email:', this.form.value.email);
     
-    console.log('phone:', this.contactForm.value.phone);
+    console.log('phone:', this.form.value.phone);
     
-    console.log('message:', this.contactForm.value.message);
+    console.log('message:', this.form.value.message);
     
   }
 
   haveErrorsInputForm(input: string, type: string) {
     return Boolean(
-      this.contactForm.get(input)?.hasError(type) && this.contactForm.get(input)?.touched
+      this.form.get(input)?.hasError(type) && this.form.get(input)?.touched
     );
   }
 
