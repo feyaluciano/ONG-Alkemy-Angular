3dc3ee63a08f6119b2572ca12b37a3c670bae17f
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-11 17:29:13 -0300

    OT97-106/features/authStore-authReducer StoreModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './features/material/material.module';
+import { StoreModule } from '@ngrx/store';
+import { authReducer } from './core/redux/authReducer.reducer';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { MaterialModule } from './features/material/material.module';
     FeaturesModule,
     SharedModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({ authReducer: authReducer })
     
   ],
   exports: [],
