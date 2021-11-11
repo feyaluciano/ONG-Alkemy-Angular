59030306c1eb788c64dfce9534e3d6722a37fe07
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-11 17:35:54 -0300

    OT97-106/features/authStore-authReducer I injected store in loginComponent

--- src/app/features/public/pages/auth/login-form/login-form.component.ts
+++ src/app/features/public/pages/auth/login-form/login-form.component.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { login } from 'src/app/core/redux/authReducer.actions';
+
 import { AuthService } from 'src/app/core/services/auth.service';
 import { environment } from 'src/environments/environment';
 import { User } from '../../../../models/User';
@@ -11,19 +15,22 @@ import { User } from '../../../../models/User';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent  {
+export class LoginFormComponent implements OnInit  {
   title = 'branchJose';
   forma:FormGroup;
   urlApi = environment.apiUrl;
   userLogin:string ="";
   token:string = "";
 
-  constructor(private fb:FormBuilder, private httpClient:HttpClient, private authServices:AuthService, private Router:Router){
+  constructor(private fb:FormBuilder, private httpClient:HttpClient, private authServices:AuthService, private Router:Router, private store: Store){
       this.forma = this.fb.group({
       email:   ['test@demo.com', [Validators.required, Validators.email]],
       password: ['@Cepita67',  [Validators.required, Validators.minLength(6), 
       Validators.pattern(/^(?=.*[A-Za-z])(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/)]],
     });
+  }
+  ngOnInit(): void {
+    this.store.subscribe(  console.log );
   }
 
   
@@ -42,9 +49,14 @@ export class LoginFormComponent  {
         password: this.forma.get('password')?.value
     }
 
+    
+
     this.authServices.auth(user).subscribe((resp:any)=>{
       this.userLogin = resp.data.user;
       this.token = resp.data.token;
+
+      this.store.dispatch(login({user: resp.data.user, token: resp.data.token}));
+
       localStorage.setItem("userToken", JSON.stringify(this.token));
       localStorage.setItem("user", JSON.stringify(this.userLogin));
       this.Router.navigate(["/dashboard"])
