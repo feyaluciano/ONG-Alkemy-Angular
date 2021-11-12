4bf73b36917cb7f352ddcabd659a458deb1f2176
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2021-11-12 17:33:04 -0300

    Delete environment.local.ts
    Delete local environment

--- src/environments/environment.local.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl:"http://ongapi.alkemy.org/api",
-    activitiesApiUrl: "http://ongapi.alkemy.org/api/activities/",
-    membersApiUrl: "http://ongapi.alkemy.org/api/members/",
-    slidesApiUrl: "http://ongapi.alkemy.org/api/slides/",
-    testimonialsApiUrl: "http://ongapi.alkemy.org/api/testimonials",
-    newsApiUrl: "http://ongapi.alkemy.org/api/news",
-  };
