f4213e520e4562d9a68d37c0a4c909ed0738df55
Luciano <feyaluciano@gmail.com>
2021-11-15 01:24:24 -0300

    OT97-108/Luciano-i added the reducer activities

--- src/app/core/redux/actions/auth.actions.ts
+++ src/app/core/redux/actions/auth.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { Activity } from 'src/app/features/models/Activity';
 import { User } from '../../../features/models/User';
 
 
@@ -22,5 +23,11 @@ export const register   = createAction('[Authentication] Register', props<IAuthP
 export const logout     = createAction('[Authentication] Logout');
 
 export const setAuthState = createAction('[Authentication] Auth State Modified', props<IAuthPropsResponse>() );
+
+
+export const findAllActivities        = createAction('[ Activity ] Find All Activities');
+export const findAllActivitiesSuccess = createAction('[ Activity ] Find All Activities Success', props<{ payloadActivity: Array<Activity> }>());
+export const findAllActivitiesError = createAction('[ Activity ] Find All Activities Error',props<{ error: any }>());
+
 
 //export const loginError = createAction('[Authentication] Auth State Error', props<any>());
