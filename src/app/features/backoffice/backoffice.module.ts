729d6445da88dfab7f029abf82fc6440de6f6990
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-15 23:35:04 -0300

    User Form Component Declared

--- src/app/features/backoffice/backoffice.module.ts
+++ src/app/features/backoffice/backoffice.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { BackofficeRoutingModule } from './backoffice-routing.module';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "src/app/shared/shared.module";
-
-import { ActivityFormComponent } from './pages/activity-form/activity-form.component';
+import { MaterialModule } from '../material/material.module';
+import { AppBackofficeComponent } from './app-backoffice.component';
+import { BackofficeRoutingModule } from './backoffice-routing.module';
+import { HeaderBackofficeComponent } from './components/header-backoffice/header-backoffice.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ActivityFormComponent } from './pages/activity-form/activity-form.component';
 import { BackofficeHomeComponent } from './pages/backoffice-home/backoffice-home.component';
 import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
-
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MemberFormComponent } from './pages/member-form/member-form.component';
-import { OrganizationComponent } from './pages/organization/organization.component';
-import { AppBackofficeComponent } from './app-backoffice.component';
-import { HeaderBackofficeComponent } from './components/header-backoffice/header-backoffice.component';
-import { SlideComponent } from './pages/slide/slide.component';
-
-import { OrganizationEditComponent } from './pages/organization-edit/organization-edit.component';
 import { NewsFormComponent } from './pages/news/news-form/news-form.component';
-import { MaterialModule } from '../material/material.module';
-import { UserslistComponent } from './pages/userslist/userslist.component';
+import { OrganizationEditComponent } from './pages/organization-edit/organization-edit.component';
+import { OrganizationComponent } from './pages/organization/organization.component';
+import { SlideComponent } from './pages/slide/slide.component';
 import { SlidesComponent } from './pages/slides/slides.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
+import { UserslistComponent } from './pages/userslist/userslist.component';
+
+
+
+
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { SlidesComponent } from './pages/slides/slides.component';
     OrganizationComponent,
     NewsFormComponent,
     UserslistComponent,
-    SlidesComponent
+    SlidesComponent,
+    UserFormComponent
     
     
   ],
