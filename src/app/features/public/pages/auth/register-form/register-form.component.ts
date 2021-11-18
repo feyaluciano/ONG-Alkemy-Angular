d29e69c3d8f126dbd614490057788bea633a4269
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-18 14:50:54 -0300

    OT97-121 Terms and Conditions Ok

--- src/app/features/public/pages/auth/register-form/register-form.component.ts
+++ src/app/features/public/pages/auth/register-form/register-form.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { UserStatusService } from "src/app/core/services/user-status.service";
 import { User } from "../../../../models/User";
+import { MatDialog } from '@angular/material/dialog';
+import { StandarDialogComponent } from "src/app/shared/components/standar-dialog/standar-dialog.component";
 
 @Component({
   selector: "app-register-form",
@@ -13,7 +15,14 @@ export class RegisterFormComponent implements OnInit {
   form: FormGroup;
   editing: boolean = false;
   passwordsAreEqualValue: boolean = false;
-  constructor(private userStatusService:UserStatusService,private _builder: FormBuilder, private router: Router) {
+  termsOk: boolean = false;
+  
+  constructor(
+    private userStatusService: UserStatusService,
+    private _builder: FormBuilder,
+    private router: Router,
+    public dialog: MatDialog
+  ) {
     this.form = this._builder.group({
       email: ["ejemplo@gmail.com", [Validators.required, Validators.email]],
       password: [
@@ -44,9 +53,18 @@ export class RegisterFormComponent implements OnInit {
   }
 
   async save() {
+
+    if (this.termsOk === false) {
+      let dialogRef = this.dialog.open(StandarDialogComponent, {
+        height: '300px',
+        width: '400px',
+        data: {type: "noTerms", titleToShow:"",messageToShow: "Debe leer y aceptar los términos y condiciones de la ONG",showButtonsOkCancel:false},
+      });            
+      dialogRef.afterClosed().subscribe(result => { });
+    }
     
     this.passwordsAreEqualValue = !this.passwordsAreEqual();
-    if (this.form.valid && this.passwordsAreEqual()) {
+    if (this.form.valid && this.passwordsAreEqual() && this.termsOk) {
       const user: User = {
         email: this.form.get("email")?.value,
         password: this.form.get("password")?.value,
@@ -60,5 +78,16 @@ export class RegisterFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.editing = false;
+  }
+
+  readTerms() {
+    let dialogRef = this.dialog.open(StandarDialogComponent, {
+      height: '80vh',
+      width: '60%',
+      data: {type: "terms", titleToShow:"",messageToShow: "",showButtonsOkCancel:false},
+    });            
+    dialogRef.afterClosed().subscribe(result => {
+      this.termsOk = result === 'cancelTerms' || !result ? false : true; 
+    });
   }
 }
