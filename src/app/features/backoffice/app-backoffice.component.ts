ff7751d9f75d0622e2f4f739ecf24c3f3087b3e7
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-22 10:43:03 -0300

    OT97-134/Angular Animations added

--- src/app/features/backoffice/app-backoffice.component.ts
+++ src/app/features/backoffice/app-backoffice.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { fadeInAnimation } from 'src/app/shared/animations/animations';
 
 @Component({
   selector: 'app-app-backoffice',
   templateUrl: './app-backoffice.component.html',
-  styleUrls: ['./app-backoffice.component.scss']
+  styleUrls: ['./app-backoffice.component.scss'],
+  animations: [
+    fadeInAnimation
+    // animation triggers go here
+  ]
 })
 export class AppBackofficeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 
 }
