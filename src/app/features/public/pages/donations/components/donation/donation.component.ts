f85c33c8b83471fb2026656a3eb2ef467d501c1e
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-22 20:23:56 -0300

    OT97-188/features/MercadoPago - I removed <p> in donationComponent

--- src/app/features/public/pages/donations/components/donation/donation.component.ts
+++ src/app/features/public/pages/donations/components/donation/donation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
 import { CheckoutproService } from 'src/app/features/services/mercadopago/checkoutpro.service';
+
 
 @Component({
   selector: 'app-donation',
