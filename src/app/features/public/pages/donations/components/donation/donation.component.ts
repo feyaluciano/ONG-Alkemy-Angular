fa5ed3bebfe1bd8d9c8cfa987b573e22620f2306
Franco Oliva <franco.oliva1991@gmail.com>
2021-11-22 13:34:13 -0300

    OT97-188/features/MercadoPago - button testing() in donation component

--- src/app/features/public/pages/donations/components/donation/donation.component.ts
+++ src/app/features/public/pages/donations/components/donation/donation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CheckoutproService } from 'src/app/features/services/mercadopago/checkoutpro.service';
 
 @Component({
   selector: 'app-donation',
@@ -16,7 +17,8 @@ export class DonationComponent implements OnInit {
   
   constructor(
     private _builder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private mercadopago: CheckoutproService
   ) {
     this.form = this._builder.group({
       value: [""],      
@@ -33,6 +35,33 @@ export class DonationComponent implements OnInit {
       this.router.navigate(['/gracias']);
       return true;
     }
+  }
+
+  testing(){
+
+    let preference = {
+      external_reference:"abc",
+      items:[
+          {
+              title:"Donaciones",
+              description:"ONG Somos Más",
+              quantity:1,
+              unit_price:10000,
+              picture_url:"https://tienda.capellansf.net/wp-content/uploads/2021/03/20191212-donacion.jpg"
+          }
+      ],
+      back_urls: {
+        success:"http://localhost:4200/gracias",
+        failure:"http://localhost:4200/gracias",
+        pending: ""
+      }
+  }
+    this.mercadopago.createPreference(preference).subscribe(r => {
+      console.log(r)
+
+      window.location.href = r.sandbox_init_point;
+    });
+
   }
 
 }
