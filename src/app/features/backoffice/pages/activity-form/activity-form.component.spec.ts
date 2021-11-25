5d71c92a2744cf05b86dc4546134b0494124a9c4
Luciano <feyaluciano@gmail.com>
2021-11-25 14:53:26 -0300

    i removed a line

--- src/app/features/backoffice/pages/activity-form/activity-form.component.spec.ts
+++ src/app/features/backoffice/pages/activity-form/activity-form.component.spec.ts
@@ -1,36 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { FormBuilder, Validators } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import {  of } from 'rxjs';
 import { AppModule } from 'src/app/app.module';
-import { CoreModule } from 'src/app/core/core.module';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { FeaturesModule } from 'src/app/features/features.module';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { BackofficeRoutingModule } from '../../backoffice-routing.module';
-import { BackofficeModule } from '../../backoffice.module';
-
 
 //https://medium.com/@jorgeucano/introducci%C3%B3n-al-testing-en-angular-da415ef8c47
 
 import { ActivityFormComponent } from './activity-form.component';
-
-
-
-
 describe('ActivityFormComponent', () => {
-  let  _builder:FormBuilder=new FormBuilder();
+  
 let idActivityInRoute:string;
 
-  const activatedRouteSpy = {
-    snapshot: {
-      paramMap: convertToParamMap({
-        idActivity: '1',       
-      })
-    }
-  };
+  
 
     beforeEach((() => {
       TestBed.configureTestingModule({
@@ -49,7 +32,7 @@ let idActivityInRoute:string;
             snapshot: {
               parent: {
                 params: {
-                  idActivity: '1'
+                  idActivity: '22'
                 }
               },
               paramMap: {
@@ -63,7 +46,17 @@ let idActivityInRoute:string;
       
       ],        
       }).compileComponents();
-    }));   
+    }));  
+   it('should have a parameter idActivity and tobe a number',  async() => {
+    const fixture = TestBed.createComponent(ActivityFormComponent);
+    const componentAct = fixture.componentInstance; 
+    componentAct.route.params.subscribe((params) => {
+      idActivityInRoute = params.idActivity;
+     expect(idActivityInRoute).toMatch(/^([0-9])*$/);
+  });
+  });
+    
+    
   it('should create', () => {
     const fixture = TestBed.createComponent(ActivityFormComponent);
     const appAct = fixture.componentInstance;
@@ -79,47 +72,9 @@ let idActivityInRoute:string;
     expect(componentAct.form.invalid).toBeFalse();
   });
 
-  it('should have a parameter idActivity',  async() => {
-    const fixture = TestBed.createComponent(ActivityFormComponent);
-    const componentAct = fixture.componentInstance; 
-    componentAct.route.params.subscribe((params) => {
-      idActivityInRoute = params.idActivity;
-     expect(idActivityInRoute).toMatch(/^([0-9])*$/);
-  });
-
-
-    // componentAct.route.snapshot.params.get('idActivity').then((data:string)=>{
-    //   console.error("daaaaaaaa"+data)
-    // })
-
-    //componentAct.route.snapshot.queryParamMap.get('id');
-
-//     const idActivity=Number(componentAct.route.snapshot.params.get('idActivity'))
-// const idd=1;
-//     //expect(componentAct.route.snapshot.paramMap.get('idActivity')).toEqual(jasmine.any(Number));
-//     tick();
-//    // expect(componentAct.route.snapshot.paramMap.get('idActivity')).toMatch(/\d{1,}/);  
-// console.log("xxxxxxxxxx"+componentAct.route.snapshot.params.get('idActivity'))
-//    expect(typeof componentAct.route.snapshot.paramMap.get('idActivity')).toEqual('number');
-   //expect(componentAct.route.snapshot.paramMap.get('idActivity')).toBeGreaterThan(0);
-
-    //expect(  componentAct.routeIs.snapshot.params["idActivity"].toBeDefined()   );      
-  });
-
-
-  // it('should have a parameter idActivity and to be a number',  async() => {
-  //   const fixture = TestBed.createComponent(ActivityFormComponent);
-  //   const componentAct = fixture.componentInstance;     
-    
-  //   expect(  (componentAct.routeIs.snapshot.params["idActivity"].toBeDefined()) &&  (componentAct.routeIs.snapshot.params["idActivity"].isNumber)   );
-
-  // });
-
-
-  //ng test --include src/app/features/backoffice/pages/activity-form/activity-form.component.spec.ts
-
   
   
+  //ng test --include src/app/features/backoffice/pages/activity-form/activity-form.component.spec.ts    
 });
 
 
