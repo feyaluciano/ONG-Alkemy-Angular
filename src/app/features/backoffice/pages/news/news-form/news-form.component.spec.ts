86f9cd505fb4ec3e02361f9a7bb13c62d400be73
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-11-28 17:40:25 -0300

    OT97-151/1st. Attempt

--- src/app/features/backoffice/pages/news/news-form/news-form.component.spec.ts
+++ src/app/features/backoffice/pages/news/news-form/news-form.component.spec.ts
@@ -1,16 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NewsFormComponent } from './news-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NewsFormComponent', () => {
+
   let component: NewsFormComponent;
-  let fixture: ComponentFixture<NewsFormComponent>;
+  let router: Router;
+  let fixture;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NewsFormComponent ]
+    TestBed.configureTestingModule({
+      declarations: [ NewsFormComponent ],
+      imports: [
+        // HttpClientModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+        // ... whatever module you have
+      ],
+      providers:
+      [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {params: {id: '1052'}}
+          }
+        }
+      ]
     })
     .compileComponents();
+
+    // router = TestBed.get(Router);
   });
 
   beforeEach(() => {
@@ -19,7 +40,17 @@ describe('NewsFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('If ID is provided, set text value', <any>fakeAsync(() => {
+    router.navigate(['/backoffice/news', '1052']);
+    tick(500);
+    expect(component.titlePage).toBe('Formulario Edición de Novedades');
+  }));
+  // A simple test:
+  // it('the sum 1 + 1 should be 2', () => {
+  //   expect(1+1).toEqual(2);
+  // });
 });
