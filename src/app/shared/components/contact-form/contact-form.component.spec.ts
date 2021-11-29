a05bd952a9079e83408e39cc6d4a572fc129af61
Jose Brito <ing.josebritovillarroel@gmail.com>
2021-11-29 20:43:43 -0300

    testing form contact call http success

--- src/app/shared/components/contact-form/contact-form.component.spec.ts
+++ src/app/shared/components/contact-form/contact-form.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { ContactFormComponent } from './contact-form.component';
+import { ContactFormService } from './contact-form.service';
 
 describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
   let fixture: ComponentFixture<ContactFormComponent>;
+  let services: ContactFormService;
+  let httpclientSpy: {post:jasmine.Spy};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +21,11 @@ describe('ContactFormComponent', () => {
     fixture = TestBed.createComponent(ContactFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    httpclientSpy = jasmine.createSpyObj('http', ['post']);
+    services = new ContactFormService(httpclientSpy as any)
   });
 
   it('should create', () => {
@@ -44,10 +53,47 @@ describe('ContactFormComponent', () => {
     expect( componentContact.send).toBeFalse();
   })
 
-  it('probar la correcta peticion http', ()=>{
+  it('No enviar el formulario sino se completo', ()=>{
     const fixture = TestBed.createComponent(ContactFormComponent);
     const  componentContact = fixture.componentInstance;
-
-    
+    componentContact.contactForm.get("name")!.setValue("");
+    componentContact.contactForm.get("email")!.setValue("");
+    componentContact.contactForm.get("phone")!.setValue("");
+    componentContact.contactForm.get("message")!.setValue("");
+    expect( componentContact.send).toBeFalse();
   })
+
+  it('probar la correcta peticion http', (done:DoneFn)=>{
+
+    let contacto = {
+      name: 'jose',
+      email: 'test@demo.com',
+      phone: '985202334',
+      message: 'prueba',
+      updated_at: "2021-11-29T22:51:42.000000Z",
+      created_at: "2021-11-29T22:51:42.000000Z",
+    }
+    
+    const mockContact = 
+      {
+        "success": true,
+        "data": {
+            "email": "test@demo.com",
+            "name": "jose",
+            "phone": "985202334",
+            "message": "prueba",
+            "updated_at": "2021-11-29T22:51:42.000000Z",
+            "created_at": "2021-11-29T22:51:42.000000Z",
+        },
+        "message": "Contact saved successfully"
+    }
+        
+    httpclientSpy.post.and.returnValue(of(mockContact));
+
+    services.contactPost(contacto).subscribe((resp:any)=>{
+      expect(resp.success).toBeTrue();
+      done()
+    })
+  })
+
 });
--- src/app/shared/components/contact-form/contact-form.service.ts
+++ src/app/shared/components/contact-form/contact-form.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactFormService {
+  contactData  = [];
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+  
+  }
+
+  contactPost(data:any){
+   return this.http.post('http://ongapi.alkemy.org/api/contacts', data)
+  }
+
+
+
+
 }
